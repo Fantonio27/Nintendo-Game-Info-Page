24ee13a7e6f3c095d627c61476260dbe9f069f45
Francis <francis.antonio0927@gmail.com>
2025-03-25 22:16:20 +0800

    Initial commit

--- src/views/GamingInfo.vue
+++ src/views/GamingInfo.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <div>
+    <Product />
+    <GameDescription />
+    <OtherInformation />
+    <GameDetails />
+    <Features />
+    <MoreLikeThis />
+    <Disclaimer />
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import Product from "../components/GamingInfoComponents/Product.vue";
+import GameDescription from "../components/GamingInfoComponents/GameDescription.vue";
+import OtherInformation from "../components/GamingInfoComponents/OtherInformation.vue";
+import GameDetails from "../components/GamingInfoComponents/GameDetails.vue";
+import Features from "../components/GamingInfoComponents/Features.vue";
+import MoreLikeThis from "../components/GamingInfoComponents/MoreLikeThis.vue";
+import Disclaimer from "../components/GamingInfoComponents/Disclaimer.vue";
+
 export default {
-
-}
+  components: {
+    Disclaimer,
+    Features,
+    GameDescription,
+    GameDetails,
+    MoreLikeThis,
+    OtherInformation,
+    Product,
+  },
+};
 </script>
-
-<style>
-
-</style>
