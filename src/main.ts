45bef872b46ef2df0502bd7360361974a7f368ed
Francis <francis.antonio0927@gmail.com>
2025-04-01 21:17:24 +0800

    Register Icon component globally in main application file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.scss'
-import App from './App.vue'
+import App from './App.vue';
+import { Icon } from "@iconify/vue";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component("Icon", Icon);
+app.mount('#app');
