4cddc7e47dc43fad1e1952cffea772bd0aa42ce9
Francis <francis.antonio0927@gmail.com>
2025-04-01 21:16:45 +0800

    Refactor Navbar component to include TopNavbar and BottomNavbar

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -1,10 +1,19 @@
 <template>
-  
+  <div class="navbar">
+    <TopNavbar />
+    <BottomNavbar />
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import TopNavbar from "./NavbarComponents/TopBar.vue";
+import BottomNavbar from "./NavbarComponents/BottomBar.vue";
 
+export default {
+    components: {
+        TopNavbar,
+        BottomNavbar
+    }
 }
 </script>
 
